cd4196a928445134314cc932894bbaf69dbf60cd
Yamapi0103 <38974693+Yamapi0103@users.noreply.github.com>
2020-08-31 14:31:14 +0800

    首頁

---
+++ src/components/header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="header">
+    <div class="logo">
+      <img src="@/assets/images/fb_logo.svg" />
+    </div>
+    <div class="banner">
+      <!-- <img src="@/assets/images/banner.png" /> -->
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+#header {
+  position: sticky;
+  top: 0;
+  margin: 0 auto;
+  div.logo {
+    padding: 10px;
+    width:100%;
+    background: white;
+    img {
+      width: 150px;
+      height: auto;
+    }
+  }
+  div.banner {
+    height:147px;
+    background: url('../assets/images/banner.png') no-repeat center center;
+    background-size: cover;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="home">
+    <Header></Header>
+    <div class="wrap">
+      <div class="block">
+        <div class="no-text header"></div>
+        <div class="icon_text_wrap">
+          <img class="white_menu02" src="@/assets/images/white_menu_02.svg" />
+          <p>
+            多張保單地址 / 電話 /
+            <br>Email變更
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from "@/components/header";
+  export default {
+    components: {
+      Header,
+    },
+  };
+</script>
+<style lang="scss">
+#home {
+  height: 100vh;
+}
+.wrap {
+  margin: 0 auto;
+  width: 1200px;
+  background: white;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  padding: 50px;
+  box-shadow: 0px 5px 5px #888;
+  .block {
+    border: 2px solid #008795;
+    border-radius: 0 0 5px 5px;
+    .no-text.header {
+      height: 10px;
+      background: #008795;
+    }
+    .icon_text_wrap {
+      display: flex;
+      border-bottom: 1px solid #008795;
+      margin: 15px;
+      img.white_menu02 {
+        width: 50px;
+      }
+    }
+  }
+}
+</style>
